69bf3f43188870d086b91827196e3d137d9e9df2
DarkLight0418 <samseong0418@naver.com>
2026-01-22 18:47:25 +0900

    20260122 엔티티 테스트

---
+++ devTracker_backend/src/main/java/khj/DevTracker/domain/member/entity/Member.java
@@ -0,0 +1,20 @@
+package khj.DevTracker.domain.member.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 회원 엔티티 클래스
+ * @author kcw
+ * @since 2026-01-21
+ */
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+
+}
