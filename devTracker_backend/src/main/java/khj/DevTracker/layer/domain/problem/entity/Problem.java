de72e01f788f77088000bc1b4dc2bbc90c790a35
DarkLight0418 <samseong0418@naver.com>
2026-02-10 09:53:48 +0900

    feat: problem 엔티티 임시 추가

---
+++ devTracker_backend/src/main/java/khj/DevTracker/layer/domain/problem/entity/Problem.java
@@ -0,0 +1,21 @@
+package khj.DevTracker.layer.domain.problem.entity;
+
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 문제 엔티티 클래스
+ * @author khj
+ * @since 2026-02-10
+ */
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Problem {
+
+}
